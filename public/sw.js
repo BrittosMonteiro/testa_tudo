4a732cababfce1c5f7be5fcdd970f6c2339d49e3
Lucas Brittos <brittosmonteiro@gmail.com>
2025-10-17 15:15:51 -0300

    fix

--- public/sw.js
+++ public/sw.js
@@ -53,43 +53,43 @@ self.addEventListener("message", (event) => {
 });
 
 // Estratégia de busca (Network falling back to Cache)
-self.addEventListener("fetch", (event) => {
-  const { request } = event;
-
-  // Ignorar requisições não GET
-  if (request.method !== "GET") return;
-
-  event.respondWith(
-    fetch(request)
-      .then((response) => {
-        // Salva no cache respostas bem-sucedidas
-        const responseClone = response.clone();
-        caches
-          .open(CACHE_NAME)
-          .then((cache) => cache.put(request, responseClone));
-        return response;
-      })
-      .catch(async () => {
-        // Busca no cache em caso de falha na rede
-        const cachedResponse = await caches.match(request);
-        if (cachedResponse) return cachedResponse;
-
-        // Retorna a página offline se nada for encontrado
-        if (request.mode === "navigate") {
-          return caches.match("/offline.html");
-        }
-      })
-  );
-});
-
 // self.addEventListener("fetch", (event) => {
-//   if (event.request.mode === "navigate") {
-//     event.respondWith(
-//       fetch(event.request).catch(async () => {
-//         const cache = await caches.open("my-app-cache-v2");
-//         const cached = await cache.match(event.request);
-//         return cached || cache.match("/offline.html");
+//   const { request } = event;
+
+//   // Ignorar requisições não GET
+//   if (request.method !== "GET") return;
+
+//   event.respondWith(
+//     fetch(request)
+//       .then((response) => {
+//         // Salva no cache respostas bem-sucedidas
+//         const responseClone = response.clone();
+//         caches
+//           .open(CACHE_NAME)
+//           .then((cache) => cache.put(request, responseClone));
+//         return response;
 //       })
-//     );
-//   }
+//       .catch(async () => {
+//         // Busca no cache em caso de falha na rede
+//         const cachedResponse = await caches.match(request);
+//         if (cachedResponse) return cachedResponse;
+
+//         // Retorna a página offline se nada for encontrado
+//         if (request.mode === "navigate") {
+//           return caches.match("/offline.html");
+//         }
+//       })
+//   );
 // });
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(async () => {
+        const cache = await caches.open("my-app-cache-v2");
+        const cached = await cache.match(event.request);
+        return cached || cache.match("/offline.html");
+      })
+    );
+  }
+});
